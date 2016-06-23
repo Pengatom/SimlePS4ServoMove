0f0d9e31514f36cc13986713791ddfb02dcbaee2
leon <rwjung@robotis.com>
2016-06-23 16:05:35 +0900

    C# example comments modified & c ver. .dll binding example - java example source uploaded

---
+++ java/protocol2.0/ping/Ping.java
@@ -0,0 +1,100 @@
+/*
+* ping.java
+*
+*  Created on: 2016. 5. 16.
+*      Author: Leon Ryu Woon Jung
+*/
+
+//
+// *********     ping Example      *********
+//
+//
+// Available Dynamixel model on this example : All models using Protocol 2.0
+// This example is designed for using a Dynamixel PRO 54-200, and an USB2DYNAMIXEL.
+// To use another Dynamixel model, such as X series, see their details in E-Manual(support.robotis.com) and edit below variables yourself.
+// Be sure that Dynamixel PRO properties are already set as %% ID : 1 / Baudnum : 3 (Baudrate : 1000000 [1M])
+//
+
+import java.util.Scanner;
+
+public class Ping
+{
+  public static void main(String[] args)
+  {
+    // Protocol version
+    int PROTOCOL_VERSION                = 2;                   // See which protocol version is used in the Dynamixel
+
+    // Default setting
+    byte DXL_ID                         = 1;                   // Dynamixel ID: 1
+    int BAUDRATE                        = 1000000;
+    String DEVICENAME                   = "COM8";              // Check which port is being used on your controller
+                                                               // ex) "COM1"   Linux: "/dev/ttyUSB0"
+
+    int COMM_SUCCESS                    = 0;                   // Communication Success result value
+    int COMM_TX_FAIL                    = -1001;               // Communication Tx Failed
+
+    // Instead of getch
+    Scanner scanner = new Scanner(System.in);
+    
+    // Initialize Dynamixel class for java
+    Dynamixel dynamixel = new Dynamixel();
+
+    // Initialize PortHandler Structs
+    // Set the port path
+    // Get methods and members of PortHandlerLinux or PortHandlerWindows
+    int port_num = dynamixel.portHandler(DEVICENAME);
+
+    // Initialize PacketHandler Structs
+    dynamixel.packetHandler();
+
+    int dxl_comm_result = COMM_TX_FAIL;             // Communication result
+    
+    byte dxl_error = 0;                             // Dynamixel error
+    int dxl_model_number;                           // Dynamixel model number
+
+    // Open port
+    if (dynamixel.openPort(port_num))
+    {
+      System.out.println("Succeeded to open the port!");
+    }
+    else
+    {
+      System.out.println("Failed to open the port!");
+      System.out.println("Press any key to terminate...");
+      scanner.nextLine();
+      return;
+    }
+
+    // Set port baudrate
+    if (dynamixel.setBaudRate(port_num, BAUDRATE))
+    {
+      System.out.println("Succeeded to change the baudrate!");
+    }
+    else
+    {
+      System.out.println("Failed to change the baudrate!");
+      System.out.println("Press any key to terminate...");
+      scanner.nextLine();
+      return;
+    }
+
+    // Try to ping the Dynamixel
+    // Get Dynamixel model number
+    dxl_model_number = dynamixel.pingGetModelNum(port_num, PROTOCOL_VERSION, DXL_ID);
+    if ((dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)) != COMM_SUCCESS)
+    {
+      dynamixel.printTxRxResult(PROTOCOL_VERSION, dxl_comm_result);
+    }
+    else if ((dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)) != 0)
+    {
+      dynamixel.printRxPacketError(PROTOCOL_VERSION, dxl_error);
+    }
+
+    System.out.printf("[ID: %d] ping Succeeded. Dynamixel model number : %d\n", DXL_ID, dxl_model_number);
+
+    // Close port
+    dynamixel.closePort(port_num);
+
+    return;
+  }
+}
